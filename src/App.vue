95c8a41c458605148d7fda3986d27629e204c8fe
Claudio Wiedenegger <cwiedenegger@student.tgm.ac.at>
2026-02-14 20:03:23 +0100

    AnimatedBackground global in App.vue integrated

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="min-h-screen bg-gray-100">
+  <!--
+    App.vue – Wurzel-Komponente der WeMood-Anwendung.
+    Beinhaltet den globalen animierten Hintergrund und das Routing.
+
+    Quellen:
+    - Vue 3 Single File Components: https://vuejs.org/guide/scaling-up/sfc.html
+    - Vue Router <router-view>: https://router.vuejs.org/guide/
+  -->
+  <div class="min-h-screen relative">
+    <!-- Globaler animierter Hintergrund -->
+    <AnimatedBackground />
+
+    <!-- Seiten-Inhalt über dem Hintergrund -->
     <router-view @open-emergency="openEmergency" />
+
+    <!-- Notfall-Modal -->
     <EmergencyModal ref="emergencyModal" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import AnimatedBackground from './components/AnimatedBackground.vue'
 import EmergencyModal from './components/EmergencyModal.vue'
 
 const emergencyModal = ref(null)
